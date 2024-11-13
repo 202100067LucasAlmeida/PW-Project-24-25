5e1237a68ba05afa99ca8ba7521aff0f36599a82
202100067LucasAlmeida <202100067@estudantes.ips.pt>
2024-11-13 15:49:15 +0000

    primeiras classes

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,2 +1,35 @@
 "use strict";
 
+class Event{
+    #name;
+
+    constructor(name){
+        this.#name = name;
+    }
+
+    get name(){
+        this.#name;
+    }
+
+    set name(newName){
+        this.#name = newName;
+    }
+}
+
+class TypesOfEvents{
+    #typeOfEvents
+
+    constructor(){
+        this.#typeOfEvents = [];
+    }
+
+    addTypeOfEvent(event){
+        (event instanceof Event) ? this.#typeOfEvents.push(event) : void 0;
+    }
+
+    removeTypeOfEvent(event){
+        let validationKey = event instanceof Event;
+        let position = (validationKey) ? this.#typeOfEvents.indexOf(event) : -1;
+        (!~position) ? this.#typeOfEvents.splice(position, 1) : void 0;
+    }
+}
