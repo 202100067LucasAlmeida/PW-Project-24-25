73b6bb977bf3af6219f2a67d73db3a279ed95108
202100067LucasAlmeida <202100067@estudantes.ips.pt>
2024-11-20 15:33:27 +0000

    Classe Member e alteração de método addTypeOfEvent

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -17,19 +17,54 @@ class Event{
 }
 
 class TypesOfEvents{
-    #typeOfEvents
+    #typeOfEvents;
 
     constructor(){
         this.#typeOfEvents = [];
     }
 
     addTypeOfEvent(event){
-        (event instanceof Event) ? this.#typeOfEvents.push(event) : void 0;
+        (!this.#typeOfEvents.some(existingEvent => existingEvent === event) && event instanceof Event) ? 
+            this.#typeOfEvents.push(event) : void 0;
     }
 
     removeTypeOfEvent(event){
         let validationKey = event instanceof Event;
         let position = (validationKey) ? this.#typeOfEvents.indexOf(event) : -1;
         (!~position) ? this.#typeOfEvents.splice(position, 1) : void 0;
+    }
+}
+
+class Member{
+    #name;
+    #favoriteEvents;
+
+    constructor(name){
+        this.#name = name;
+        this.#favoriteEvents = new TypesOfEvents();
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    set name(newName){
+        this.#name = newName;
+    }
+
+    get favoriteEvents(){
+        return this.#favoriteEvents;
+    }
+
+    addFavoriteEvents(...event){
+        for(let i = 0; i < event.length; i++){
+            this.#favoriteEvents.addTypeOfEvent(event[i]);
+        }
+    }
+
+    removeFavoriteEvents(...event){
+        for(let i = 0; i < event.length; i++){
+            this.#favoriteEvents.removeTypeOfEvent(event[i]);
+        }
     }
 }
